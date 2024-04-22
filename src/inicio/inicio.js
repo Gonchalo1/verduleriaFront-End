8a9a65c07eef95254acefd9013daa5eedd5f503f
G <gonzalocal127@gmail.com>
2024-04-22 08:47:08 -0300

    agregue un texto en la imagen de main

--- src/inicio/inicio.js
+++ src/inicio/inicio.js
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import Footer from '../footer/footer';
 import Nav from '../nav/nav';
 import inicio from './inicio.jpg';
@@ -7,9 +8,14 @@ function Inicio() {
   return (
     <div className="container-fluid">
       <Nav />
-      <div className="row">
+      <div className="row position-relative">
         <div className="col">
           <img src={inicio} className="img-fluid" alt="Inicio" />
+          <div className="position-absolute top-50 start-50 translate-middle text-center">
+            <h1 className="text-dark fs-lg-5 fs-md-4 fs-sm-3 fs-2">
+              ¡Descubre la frescura que necesitas en cada bocado! 
+            </h1>
+          </div>
         </div>
       </div>
       <Footer />
