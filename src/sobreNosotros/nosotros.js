1ade6ee8660ecfcb4e412b03891018e01c4fb144
G <gonzalocal127@gmail.com>
2024-05-18 12:14:15 -0300

    Agregue cammbios a sobre nosotros

--- src/sobreNosotros/nosotros.js
+++ src/sobreNosotros/nosotros.js
@@ -1,24 +1,31 @@
 import React from 'react';
 
-import Footer from '../footer/footer';
-
 function Nosotros() {
   return (
     <div className="container text-center mt-5">
       <h2>Acerca de Nosotros</h2>
-      <p>En Verdulería del Campo, cada día nos esforzamos por ofrecerte lo mejor de la naturaleza en cada bocado. Desde el campo hasta tu mesa, nos dedicamos a seleccionar cuidadosamente los productos más frescos y nutritivos para ti y tu familia.
-
-Nos enorgullece trabajar en estrecha colaboración con nuestros proveedores locales, asegurando así la calidad y frescura de cada verdura y fruta que llega a nuestras estanterías. Creemos firmemente en la importancia de una alimentación saludable y sabemos que la base de cualquier plato delicioso comienza con ingredientes de primera calidad.
-
-Nuestro compromiso con la excelencia no se detiene en la calidad de nuestros productos, sino que también se refleja en el excepcional servicio que brindamos a nuestros clientes. Valoramos tu confianza y nos esforzamos por superar tus expectativas en cada visita.
-
-En Verdulería del Campo, no solo vendemos alimentos, sino que también promovemos un estilo de vida saludable y sostenible. Creemos en el poder de la alimentación para transformar vidas y estamos aquí para apoyarte en tu viaje hacia una vida más saludable y feliz.
-
-¡Gracias por elegirnos y ser parte de nuestra familia! Juntos, podemos cultivar un futuro más brillante y saludable.
-
-</p>
-      <Footer/>
+      <p>
+        En Verdulería del Campo, ofrecemos productos frescos y nutritivos seleccionados cuidadosamente. Trabajamos con proveedores locales para garantizar calidad y frescura.
+        <br /><br />
+        Creemos en la alimentación saludable y en los ingredientes de calidad. Nuestro compromiso se refleja en nuestros productos y servicio.
+        <br /><br />
+        Promovemos un estilo de vida saludable y sostenible. Valoramos tu confianza y nos esforzamos por superar tus expectativas. ¡Gracias por elegirnos y ser parte de nuestra familia!
+      </p>
+      <div className="iframe-container">
+        <h2>UBICACION</h2>
+        <iframe 
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6862143.794019821!2d-78.58708904770464!3d-32.88081599999998!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x967e0ee7b7aa74b5%3A0x9b6791d829f51ec7!2sFrutas%20Y%20Verduras%20EL%20JHONNY!5e0!3m2!1ses-419!2sar!4v1716043677650!5m2!1ses-419!2sar"
+          width="300"
+          height="150"
+          title="Mapa de Frutas y Verduras EL JHONNY"
+          allowFullScreen
+          loading="lazy"
+          referrerPolicy="no-referrer-when-downgrade"
+        ></iframe>
+      </div>
+      <h5>REDES SOCIALES:</h5>
     </div>
   );
 }
+
 export default Nosotros;
