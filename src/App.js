7a13a145461abc37d70cafa8a41d5e580b81e9ee
G <gonzalocal127@gmail.com>
2024-05-22 11:14:53 -0300

    mi parte de productos(no funciona)

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import CambiarDatos from './cambiarDatos/cambiarDatos';
 import Nosotros from './sobreNosotros/nosotros';
 import Inicio from './inicio/inicio';
 import Footer from './footer/footer';
+import ProductoSeccion from './productos/productoSeccion';
 
 function App() {
   const [userId, setUserId] = useState(null);
@@ -25,11 +26,12 @@ function App() {
     <Router>
       <div className="App">
         <Routes>
-          <Route path="/perfil" element={<Perfil userId={userId} />} />
           <Route path="/" element={<Inicio />} />
           <Route path="/login" element={<Login />} />
           <Route path="/registro" element={<Registro setUserId={setUserId} />} />
+          <Route path="/producto" element={<ProductoSeccion  />} />
           <Route path="/nosotros" element={<Nosotros />} />
+          <Route path="/perfil" element={<Perfil userId={userId} />} />
           <Route path="/cambiarDatos" element={<CambiarDatos userId={userId} />} />
         </Routes>
         <Footer />
